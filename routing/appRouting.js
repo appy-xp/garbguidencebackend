58e2970f5792f56958f19a1f98cb8d22f47c5e2d
appy-xp <120022679+appy-xp@users.noreply.github.com>
2024-03-02 22:30:24 +0545

    202403021029

--- routing/appRouting.js
+++ routing/appRouting.js
@@ -1,5 +1,17 @@
 var userRouter = require("./../routes/users");
+var brandRouter = require("./../routes/brand/brand");
+var itemRouter = require("./../routes/item/item");
+var roleRouter = require("./../routes/roles/roles");
+var sizeRouter = require("./../routes/size/size");
+var staffRouter = require("./../routes/staff/staff");
+var statusRouter = require("./../routes/status/status");
 
 module.exports = function (app) {
-  app.use("/user", userRouter);
+  app.use("/api/user", userRouter);
+  app.use("/api/brand", brandRouter);
+  app.use("/api/item", itemRouter);
+  app.use("/api/role", roleRouter);
+  app.use("/api/size", sizeRouter);
+  app.use("/api/staff", staffRouter);
+  app.use("/api/status", statusRouter);
 };
